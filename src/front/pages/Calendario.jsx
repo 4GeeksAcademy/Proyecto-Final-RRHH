8fa2d6c4eb6d5ef52c9048c9bf5c5d8bd97fd1ed
Lázaro <fillaux33@gmail.com>
2026-01-12 21:02:42 +0000

    calendario

--- src/front/pages/Calendario.jsx
+++ src/front/pages/Calendario.jsx
@@ -4,7 +4,10 @@ export default function Calendario() {
       <h1 className="text-3xl mb-4 text-black">
         Página de calendario
       </h1>
-      <p>Aquí va la api</p>
+      <p>
+        <div className="calendly-inline-widget" data-url="https://calendly.com/fillaux33/30min" style="min-width:320px;height:700px;"></div>
+        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+      </p>
     </section>
   );
 }
