28fa0b4cad9e6b6e0d9a07a2db6dce02c12f1c32
Lázaro <fillaux33@gmail.com>
2026-01-21 20:32:44 +0000

    cambios

--- src/front/pages/Calendario.jsx
+++ src/front/pages/Calendario.jsx
@@ -1,15 +1,59 @@
-import { InlineWidget } from "react-calendly";
+import Cal from "@calcom/embed-react";
+import { useEffect } from "react";
+import { getCalApi } from "@calcom/embed-react";
+
 
 
 export default function Calendario() {
+  useEffect(() => {
+    getCalApi().then((cal) => {
+      cal("on", {
+        action: "bookingSuccessful",
+        callback: (e) => {
+
+
+          console.log(e);
+
+          const t = { fecha: e.detail.data.date, duración: e.detail.data.duration, organizador: e.detail.data.organizer.name }
+
+          console.log("estos son los datos recogidos", t)
+
+
+          fetch('', {
+            method: "POST",
+            body: JSON.stringify(t),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+            .then(resp => {
+              console.log(resp.ok); // Será true si la respuesta es exitosa
+              console.log(resp.status); // El código de estado 201, 300, 400, etc.
+              return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+            })
+            .then(data => {
+              // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+              console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+            })
+            .catch(error => {
+              // Manejo de errores
+              console.log(error);
+            });
+
+
+        },
+
+      });
+    });
+  }, []);
+
+  
+
   return (
     <section className="">
-      <h1 className="text-3xl mb-4 text-black">
-        Página de calendario
-      </h1>
       <div className="contenedor-calendly">
         <p>
-          <InlineWidget url="https://calendly.com/" />
+          <Cal calLink="lazaro-fillaux/15min" config={{ theme: "light" }}></Cal>
         </p>
       </div>
     </section>
