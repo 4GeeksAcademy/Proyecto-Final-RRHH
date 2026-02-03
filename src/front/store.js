2a08a50d2b0be3207c3688b8b983386c0cb60702
Lázaro <fillaux33@gmail.com>
2026-02-03 00:39:53 +0000

    cambios2

--- src/front/store.js
+++ src/front/store.js
@@ -17,134 +17,136 @@ export const initialStore = () => {
 
 export default function storeReducer(store, action = {}) {
   switch (action.type) {
-    case "login":
+    case "login": {
       return { ...store, is_active: true };
+    }
 
-    case "logout":
+    case "logout": {
       localStorage.removeItem("jwt-token");
       return { ...store, is_active: false };
+    }
 
-    case "get_users":
+    case "get_users": {
       const { usuarios } = action.payload;
-
       return {
         ...store,
         usuarios: usuarios || [],
       };
+    }
 
-    case "get_roles":
+    case "get_roles": {
       const { roles } = action.payload;
-
       return {
         ...store,
         roles: roles || [],
       };
+    }
 
-    case "get_horarios":
+    case "get_horarios": {
       const { horarios } = action.payload;
-
       return {
         ...store,
         horarios: horarios || [],
       };
+    }
 
-    case "eliminar_horario":
+    case "eliminar_horario": {
       const { id_horario } = action.payload;
-
       return {
         ...store,
         horarios: store.horarios.filter((horario) => horario.id !== id_horario),
       };
+    }
 
-    case "crear_usuario":
+    case "crear_usuario": {
       const { usuario } = action.payload;
-
       return {
         ...store,
         usuarios: [...store.usuarios, usuario]
       };
+    }
 
-    case "eliminar_rol":
+    case "eliminar_rol": {
       const { id_rol } = action.payload;
-
       return {
         ...store,
         roles: store.roles.filter((rol) => rol.id !== id_rol),
       };
+    }
 
-    case "eliminar_usuario":
+    case "eliminar_usuario": {
       const { id_usuario } = action.payload;
-
       return {
         ...store,
         usuarios: store.usuarios.filter((usuario) => usuario.id !== id_usuario),
       };
+    }
 
-    case "set_input_nameUsuario":
+    case "set_input_nameUsuario": {
       const { inputNameUsuario } = action.payload;
-
       return {
         ...store,
         inputNameUsuario: inputNameUsuario,
       };
+    }
 
-    case "set_input_apellidosUsuario":
+    case "set_input_apellidosUsuario": {
       const { inputApellidosUsuario } = action.payload;
-
       return {
         ...store,
         inputApellidosUsuario: inputApellidosUsuario,
       };
+    }
 
-    case "set_input_emailUsuario":
+    case "set_input_emailUsuario": {
       const { inputEmailUsuario } = action.payload;
-
       return {
         ...store,
         inputEmailUsuario: inputEmailUsuario,
       };
+    }
 
-    case "set_input_dniUsuario":
+    case "set_input_dniUsuario": {
       const { inputDniUsuario } = action.payload;
-
       return {
         ...store,
         inputDniUsuario: inputDniUsuario,
       };
+    }
 
-    case "set_input_telefonoUsuario":
+    case "set_input_telefonoUsuario": {
       const { inputTelefonoUsuario } = action.payload;
-
       return {
         ...store,
         inputTelefonoUsuario: inputTelefonoUsuario,
       };
+    }
 
-    case "set_input_passwordUsuario":
+    case "set_input_passwordUsuario": {
       const { inputPasswordUsuario } = action.payload;
-
       return {
         ...store,
         inputPasswordUsuario: inputPasswordUsuario,
       };
+    }
 
-    case "set_select_HorarioUsuario":
+    case "set_select_HorarioUsuario": {
       const { selectHorarioUsuario } = action.payload;
-
       return {
         ...store,
         selectHorarioUsuario: selectHorarioUsuario,
       };
+    }
 
-    case "set_select_RolUsuario":
+    case "set_select_RolUsuario": {
       const { selectRolUsuario } = action.payload;
-
       return {
         ...store,
         selectRolUsuario: selectRolUsuario,
       };
+    }
 
     default:
-      throw Error("Unknown action.");
+      return store;
   }
 }
